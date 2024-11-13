b0da5443aef3477a9da7f5f43e25783cc3e7797f
Borys-dotcom <184245222+Borys-dotcom@users.noreply.github.com>
2024-11-13 12:54:29 +0100

    solution

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    
+    let allParagraphs = $('p');
+    let lastParagraph = allParagraphs[allParagraphs.length-1];
+    // let firstParagraph = allParagraphs[0];
+
+    $(lastParagraph).after('<button id="onEnd">Na Koniec</button>');
+    
+    $('#onStart').click(function(){
+        let allParagraphs = $('p');
+        let lastParagraph = allParagraphs[allParagraphs.length-1];
+        allParagraphs[allParagraphs.length-1].remove()
+        $(allParagraphs[0]).before($(lastParagraph));
+    });
+
+    $('#onEnd').click(function(){
+        let allParagraphs = $('p');
+        let firstParagraph = allParagraphs[0];
+        allParagraphs[0].remove()
+        $(allParagraphs[allParagraphs.length-1]).after($(firstParagraph));
+    });
+});
